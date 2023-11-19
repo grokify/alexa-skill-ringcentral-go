85aa9d3e4f9e4e098b58960cb70d9d2332e0c958
John Wang <johncwang@gmail.com>
2023-11-19 06:47:49 -0800

    lint: golangci: add import alias

--- src/config/configuration.go
+++ src/config/configuration.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 
 	"github.com/grokify/ringcentral-sdk-go/rcsdk/platform"
-	"github.com/patrickmn/go-cache"
+	cache "github.com/patrickmn/go-cache"
 	log "github.com/sirupsen/logrus"
 )
 
